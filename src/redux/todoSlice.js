49ce51424e5798078ab0f49380132cc50e05a244
Bertrand <bertrand.tang.dev@gmail.com>
2025-06-25 16:42:01 +0200

    update solution to use Immer

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -1,30 +1,43 @@
 // Centralise la logique métier, ici la gestion des tâches
-
-import { createSlice } from "@reduxjs/toolkit";
+import { produce } from "immer";
 
 const initialState = [];
 
-const todoSlice = createSlice({
-  name: "todos",
-  initialState,
-  reducers: {
-    // Action pour ajouter une tâche
-    addTodo: (state, action) => {
-      return [
-        ...state,
-        { id: Date.now(), text: action.payload, completed: false },
-      ];
-    },
-    // Action pour marquer une tâche comme terminée
-    toggleTodo: (state, action) => {
-      return state.map((todo) =>
-        todo.id === action.payload
-          ? { ...todo, completed: !todo.completed }
-          : todo
-      );
-    },
-  },
+// Convention de nommage du type d'action
+const ADD_TODO = "todos/addTodo";
+const TOGGLE_TODO = "todos/toggleTodo";
+
+const todoSlice = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return produce(state, (draft) => {
+        draft.push({
+          id: Date.now(),
+          text: action.payload,
+          completed: false,
+        });
+      });
+    case TOGGLE_TODO:
+      return produce(state, (draft) => {
+        // On va cherche l'élément dont l'id === 
+        const todo = draft.find((element) => element.id === action.payload);
+        if (todo) {
+          todo.completed = !todo.completed;
+        }
+      });
+    default:
+      return state;
+  }
+};
+
+export const addTodo = (text) => ({
+  type: ADD_TODO,
+  payload: text,
 });
 
-export const { addTodo, toggleTodo } = todoSlice.actions;
-export default todoSlice.reducer;
+export const toggleTodo = (id) => ({
+  type: TOGGLE_TODO,
+  payload: id,
+});
+
+export default todoSlice;
